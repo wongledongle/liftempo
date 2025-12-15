081c6501bf9b1b6b4426cf8117ff529af4b969f1
wongledognle <artbowong@gmail.com>
2025-12-15 08:43:54 -0800

    restart

---
+++ liftempoWatch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  liftempoWatch Watch App
+//
+//  Created by Arthur Wong on 12/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ liftempoWatch Watch App/liftempoWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  liftempoWatchApp.swift
+//  liftempoWatch Watch App
+//
+//  Created by Arthur Wong on 12/15/25.
+//
+
+import SwiftUI
+
+@main
+struct liftempoWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
